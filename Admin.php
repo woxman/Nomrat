f7491613d679cd27894e594ae3e93e355f8ed7a6
woxman <ahbr791@gmail.com>
2019-01-31 12:03:20 +0330

    38

--- Admin.php
+++ Admin.php
@@ -23,7 +23,7 @@ require_once "Definitions/Con_DB.php";
 <!--------------------------End Header---------------------------------------->
 <!--------------------------Start Body---------------------------------------->
 <?php
-
+#Check______True_______UserName____Password
 $msg = "";
 if (isset($_POST['send'])) {
     $username = trim($_POST['username']);
@@ -43,7 +43,7 @@ if (isset($_POST['send'])) {
                 $_SESSION['sess_user_id'] = $row['id'];
                 $_SESSION['sess_username'] = $row['user'];
                 $_SESSION['sess_name'] = $row['email'];
-                Redirect_To('?Login=True');
+                Redirect_To('Admin.php');
             } else {
                 $msg = "شناسه و پسورد اشتباه است!";
             }
@@ -54,57 +54,58 @@ if (isset($_POST['send'])) {
         $msg = "Both fields are required!";
     }
 }
+#Check______True_______UserName____Password
+#Logout_________________________________________________________________User
+if (isset($_REQUEST['False']))
+{
+    session_destroy();
+    redirect("Admin.php");
+
+}
+#Logout_________________________________________________________________User
+
 
 #-------------------------------------------------------------------
-#Logout_________________________________________________________________User
-switch (isset($_REQUEST['Login'])) {
-    case 'True':
-        if (isset($_REQUEST['False'])) {
-            session_destroy();
-            echo("<meta http-equiv=\"refresh\" content=\"0\">");
-            header('Refresh: 1; url=Admin.php');
-
-        }
-#Logout_________________________________________________________________User
-if (isset($_SESSION['sess_user_id'])) {
+if (isset($_SESSION['sess_user_id']))
+{
     echo '<h1>Welcome ' . $_SESSION['sess_name'] . '</h1>';
     echo '<a href="?False"><button>Logout</button></a>';
-} else {
-
-    }
-        break;
+}
+else
+{
+    echo("<div class='body' dir='rtl' align='center'>");
+    echo("    <div class='form bg-light'>");
+    echo("        <div class='h_form bg-light'>$A_T_1</div>");
+    echo("        <div class='img_l'>");
+    echo("            <img src='Contact/Logo/logo.png' alt='' class='img_l'>");
+    echo("        </div>");
+    echo("        <form method='post' action='' class='form-group form_c'>");
+    echo("            <div class=''>");
+    echo("                <label for='username' class='control-label'>$A_U_1</label>");
+    echo("                <div class='col-sm-11'>");
+    echo("                    <input type='text' class='form-control form_f' id='username'  name='username' required dir='ltr'>");
+    echo("                </div>");
+    echo("            </div>");
+    echo("            <div class='form-group'>");
+    echo("                <label for='password' class='control-label'>$A_P_1</label>");
+    echo("                <div class='col-sm-11'>");
+    echo("                    <input type='password' class='form-control form_f' id='password'  name='password'  required dir='ltr'>");
+    echo("                </div>");
+    echo("            </div>");
+    echo("            <div class='form-group btn_f' align='center'>");
+    echo("                <div class='col-sm-offset-2 col-sm-5 ' >");
+    echo("                    <button type='submit' class='btn btn-primary ' id='send' name='send'>$A_S_1</button>");
+    echo("                </div>");
+    echo("            </div>");
+    echo("            <span class='loginMsg alert-danger alert-link '>$msg</span>");
+    echo("            <a href='Recover.php'><p>$A_R_1</p></a>");
+    echo("        </form>");
+    echo("    </div>");
 }
 
-?>
-<div class="body" dir="rtl" align="center">
-    <div class="form bg-light">
-        <div class="h_form bg-light"><?php print ($A_T_1)?></div>
-        <div class="img_l">
-            <img src="Contact/Logo/logo.png" alt="" class="img_l">
-        </div>
-        <form method="post" action="" class="form-group form_c">
-            <div class="">
-                <label for="username" class="control-label"><?php echo($A_U_1) ?></label>
-                <div class="col-sm-11">
-                    <input type="text" class="form-control form_f" id="username"  name="username" required dir="ltr">
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="password" class="control-label"><?php echo($A_P_1) ?></label>
-                <div class="col-sm-11">
-                    <input type="password" class="form-control form_f" id="password"  name="password"  required dir="ltr">
-                </div>
-            </div>
-            <div class="form-group btn_f" align="center">
-                <div class="col-sm-offset-2 col-sm-5 " >
-                    <button type="submit" class="btn btn-primary " id="send" name="send"><?php echo($A_S_1) ?></button>
-                </div>
-            </div>
-            <span class="loginMsg alert-danger alert-link "><?php echo @$msg;?></span>
-            <a href="Recover.php"><p><?php echo($A_R_1) ?></p></a>
-        </form>
-    </div>
 
+
+?>
 <!--------------------------End Body---------------------------------------->
 <!--------------------------Start Footer---------------------------------------->
 <?php #print_r(Copy_Right());?>
